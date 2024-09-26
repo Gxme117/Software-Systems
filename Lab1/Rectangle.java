8508c19b4effcef8c6151a835b67f490ee3abc28
c3618282 <g.francois8777@student.leedsbeckett.ac.uk>
2024-09-26 14:58:17 +0100

    lab1

---
+++ Lab1/Rectangle.java
@@ -0,0 +1,77 @@
+package Lab1;
+
+public class Rectangle extends Shape {
+
+    private int width;        // gets size of one side of rectangle
+    private int height;       // gets size of second side of rectangle
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+
+    public int getArea() {
+        return (width * height);
+    }
+
+    Rectangle(int sides) {
+        super(sides);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Constructor.
+ *
+ * @param sides the number of sides within the shape.
+ */
+        Rectangle(int sides) {
+            super(sides);
+        }
+    }
+
+}
