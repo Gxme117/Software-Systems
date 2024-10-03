c88ee24bcdc77b757ed8fe55bf7408b867242e9b
Guillaume <g.francois8777@student.leedsbeckett.ac.uk>
2024-10-03 22:21:39 +0100

    changes to project structure changed packages to directories

---
+++ Lab1/src/Rectangle.java
@@ -0,0 +1,31 @@
+public class Rectangle extends Shape {
+
+    private int width;        // gets size of one side of rectangle
+    private int height;       // gets size of second side of rectangle
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+
+    public int getArea() {
+        return (width * height);
+    }
+
+    Rectangle(int sides) {
+        super(sides);
+    }
+
+}
